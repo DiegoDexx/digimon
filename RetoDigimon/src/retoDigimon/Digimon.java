a1d282d5e5a8728850e5e60ee8ce0d24870efb11
RubenMDAW <martiruben96@gmail.com>
2022-03-10 11:43:28 +0100

    Primera subida

---
+++ RetoDigimon/src/retoDigimon/Digimon.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package retoDigimon;
+
+/**
+ *
+ * @author Rubén
+ */
+public class Digimon {
+    String nombreDig;
+    String nombreevolucionDig;
+    int nivelDig;
+    int defensaDig;
+    int ataqueDig;
+    int tipoDig;
+    String fotoDig;
+    String fotovicDig;
+    String fotoderDig;
+    
+    Digimon(){
+        
+    }
+}
---
+++ RetoDigimon/src/retoDigimon/retoDigimon.java
@@ -0,0 +1,16 @@
+package retoDigimon;
+
+/**
+ *
+ * @author Rubén, Inma, Diego & Jaime
+ */
+public class retoDigimon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola");
+    }
+    
+}
